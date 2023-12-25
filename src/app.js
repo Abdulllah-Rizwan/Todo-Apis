cfda5b4efbf50e0d2ce2f7d7094c116b36764753
Abdulllah-Rizwan <abdullahrizwan354@gmail.com>
2023-12-25 19:00:18 +0500

    included helmet middleware for basic security purposes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
+import cookieParser from 'cookie-parser';
 import { api } from './routes/api.js';
 import express from 'express';
+import helmet from 'helmet';
+
 
 export const app = express();
 
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
+app.use(cookieParser());
 
 //Routes
 app.use('/v1',api);
